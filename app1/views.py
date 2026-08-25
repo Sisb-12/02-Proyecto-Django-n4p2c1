61787dcaaef1ad0df024b8c61f10e3edc0b368cc
Sisb-12 <sebastian.sandoval43@inacapmail.cl>
2026-08-25 09:14:38 -0400

    app2 terminada

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def v1_app1(request):
+    return HttpResponse("<h1>Vista 1 - App 1</h1><p>Todo a tu alcance</p>")
+
+def v2_app1(request):
+    return HttpResponse("<h1>Vista 2 - App 1</h1>")
---
+++ app2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def v1_app2(request):
+    return HttpResponse("<h1>Vista 1 - App 2</h1>")
+
+def v2_app2(request):
+    return HttpResponse("<h1>Vista 2 - App 2</h1>")
+
